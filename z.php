8fdca9a509ca49e047ffefcdece7b6f3742fa60a
katemslow <46392314+katemslow@users.noreply.github.com>
2019-11-24 17:24:06 +0700

    Create z.php

---
+++ z.php
@@ -0,0 +1,38 @@
+html>
+
+  <head>
+	<title>Hacked by Jiwa_TerSakiti BrokenHeart</title>
+	<link rel="icon" type="image/png " href="https://i.ytimg.com/vi/BlWQGDXK0M0/maxresdefault.jpg" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+	<meta name="keywords" content="Jiwa_TerSakiti BrokenHeart">
+	<meta name="description" content="GHOST_ExPloIteR_TEAM">
+	<STYLE type=text/css>*{margin:0;padding:0;-webkit-box-sizing:border-box;-moz-box-sizing:border-box;-ms-box-sizing:border-box;-o-box-sizing:border-box;box-sizing:border-box}body,html{margin:0;padding:0;font:16px/1.4 Lato,Homenaje;color:#fefeff;-webkit-font-smoothing:antialiased;font-smoothing:antialiased;font-family:Homenaje MS}body{background:black;-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}h1{font:2.75em Cinzel,serif;font-weight:400;letter-spacing:.35em;text-shadow:0 0 25px rgba(254,254,255,.85)}h2{font:1.45em Cinzel,serif;font-weight:400;letter-spacing:.5em;text-shadow:0 0 25px rgba(254,254,255,.85);text-transform:lowercase}[class^=letter]{-webkit-transition:opacity 3s ease;-moz-transition:opacity 3s ease;transition:opacity 3s ease}.letter-0{transition-delay:.2s}.letter-1{transition-delay:.4s}.letter-2{transition-delay:.6s}.letter-3{transition-delay:.8s}.letter-4{transition-delay:1s}.letter-5{transition-delay:1.2s}.letter-6{transition-delay:1.4s}.letter-7{transition-delay:1.6s}.letter-8{transition-delay:1.8s}.letter-9{transition-delay:2s}.letter-10{transition-delay:2.2s}.letter-11{transition-delay:2.4s}.letter-12{transition-delay:2.6s}.letter-13{transition-delay:2.8s}.letter-14{transition-delay:3s}h1,h2{visibility:hidden;-webkit-transform:translate3d(0,0,0);-moz-transform:translate3d(0,0,0);transform:translate3d(0,0,0)}h1.transition-in,h2.transition-in{visibility:visible}h1 [class^=letter],h2 [class^=letter]{opacity:0}h1.transition-in [class^=letter],h2.transition-in [class^=letter]{opacity:1}#container{display:table;position:absolute;z-index:20;width:100%;height:100%;text-align:center;cursor:none}#container>div{display:table-cell;vertical-align:middle}#container p{position:absolute;width:100%;left:0;bottom:25px;font-size:.8em;letter-spacing:.1em;font-weight:300;color:#76747a;-webkit-font-smoothing:subpixel-antialiased;font-smoothing:subpixel-antialiased}#container p strong{color:#b3abc5}#container p span{font-size:.75em;padding:0 2px}#canvas{position:absolute;z-index:10;top:0;left:0;width:100%;height:100%;cursor:none}#stats{position:absolute;z-index:10;left:10px;top:10px}.dg.ac{z-index:100!important}.STYLE4{color:black}</STYLE>
+	<DIV id=container>
+	  <DIV><BR><BR><BR>
+		<H3 id=h3><BR></H3><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR><BR>
+		<H3 id=h3>&nbsp; </H3></FONT></A></DIV></DIV><CANVAS id=canvas></CANVAS>
+  </head>
+  <body>
+  <body bgcolor='black'>
+	<iframe width="0" height="0" src="https://6.top4top.net/m_14038q5s01.mp3" frameborder="0" "allowfullscreen"></iframe>
+	<pre><font color="red">
+	  </font></pre><br><img style="width: 400px; height: 400px;" alt="" src="https://scontent-sin2-2.xx.fbcdn.net/v/t1.0-9/72920867_160792441676028_1841167521047117824_o.jpg?_nc_cat=111&_nc_oc=AQmZcpZKe-qFv15SiAurhKIyMr9dWsWcHMWaO7QGP2GsX14wo8kYMdODeFWkJTQqN78&_nc_ht=scontent-sin2-2.xx&oh=5c9a83ffacba9f24e0c8ad34cb82b421&oe=5E4D2612"><br><font face="Homenaje" style="font-size: 20px" color="lime"><marquee scrollamount='100' behavior='alternate' width='50%'>_______</marquee></font><p align="center"><font color="#3a9dc6" face="new rocker" size="1%">
+		<font face="Homenaje" size="2" color="#FFFFFF">
+		  <font face="Homenaje" style="color:white;text-shadow:0px 1px 5px #000;font-size:30px"><b>Hacked  by  Jiwa_TerSakiti BrokenHeart</b></font></p>
+	<p><font face="Homenaje" style="font-size: 20px" color="lime">TugasKu Hanya Mencintai Bukan Memaksa Agar Di Cin Tai </br></font></p>
+        <p><font face="Homenaje" style="font-size: 20px" color="lime"> Tolong Pak Di Negara Indonesia Ini Hacker Di Cap Sebagai Penjahat , Padahal Aslinya Kami Ini Baik Pak Tapi Sayang Pak Praturan DI NEGARA INDONESIA INI AKU MUAK DENGAN SEMUA APALAGI DPR :) </br></font></p>
+	<p><font face="Homenaje" style="font-size: 40px" color="red">G<font face="Homenaje" style="font-size: 35px" color="red">r<font face="Homenaje" style="font-size: 30px" color="red">e<font face="Homenaje" style="font-size: 25px" color="red">e<font face="Homenaje" style="font-size: 20px" color="red">t<font face="Homenaje" style="font-size: 15px" color="red">Z</font></br></font></p>
+	<p><font face="Homenaje" style="font-size: 15px" color="lime"><marquee scrollamount='6' behavior='alternate' width='50%'>-> GH05T666 -- AnoaGhost777\ -- DWL0N3 -- Mr.CroPZ -- MR-X666X // DARKNET-RUDY // ./KAIZ3N-6H05T // K1TSUN3-6H05T
+SEA-GHOST // REX4 // VENOM-GHOST // GHOST666 // Z3R0H1D3N // TUMAN-GANZ
+CHAPPiE // ./FUKUR0_3XP // NOXLAW // BABY_CODE // SADBOYS1922 // MR.DARK
+RAMIL-FEYZIYEV // D4FF4-6H05T // DARKWEBSTER // RAVINKUN@GHOST
+DEVILS'KENSHI // ./FINIX // CAPTAIN_MAXI // QUEENRA // MR.CAPUNGX6X
+CILOT_ // DENI-GANZ // RASCAL // VIN|303 // MILEA24 // R666HOST
+TN.KAL1B3R // QUEEN-GHOST // DS7 // DARK-SKY <-
+</marquee></font></p>
+	<p><font face="Homenaje" style="font-size: 20px" color="red">Katemslow@gmail.com</font>
+	  <link href='https://fonts.googleapis.com/css?family=Homenaje' rel='stylesheet'>
+	  <style>body{cursor:url("../www.madleets.com/elhacker.cur"),auto;}html{display:table;height:100%;width:100%;}body{display:table-row;}body{display:table-cell;vertical-align:middle;text-align:center;}a:link{text-decoration:none;}</style>
+	</p>
+  </body></html>
